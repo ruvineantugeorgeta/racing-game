794fa62a2e83a45cb7908d5f8c65553e4bca14a5
Ruvineantu Georgeta <ruvineantugeorgeta@gmail.com>
2020-02-24 20:47:52 +0200

    studied classes, variables, primitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,34 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the racing game" );
+
+        Car carRefrence = new Car();
+        carRefrence.name = "Audi";
+        carRefrence.color = "red";
+        carRefrence.maxSpead = 260;
+        carRefrence.mileage = 6.2;
+
+       // concatenation
+        System.out.println("Properties of " + carRefrence.name);
+        // Color: red
+        System.out.println("Color: " + carRefrence.color);
+        System.out.println("Max spead: " + carRefrence.maxSpead);
+        System.out.println("Mileage: " + carRefrence.mileage);
+        System.out.println("Racing number: "+ carRefrence.racingNumber);
+        System.out.println("Damaged: " + carRefrence.damaged);
+        System.out.println("Fuel level "+  carRefrence.fuelLevel);
+
+     carRefrence = null
+
+        Car  car2 = new Car ();
+        car2.name = "Lambourghini";
+        car2.damaged = true;
+
+        System.out.println("Properties of" + car2.name);
+        System.out.println("Color:" + car2.color);
+        System.out.println("Damaged:" +car2.damaged);
+
+
     }
 }
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,17 @@
+package org.fasttrackit;
+
+public class Car {
+    // instance variables
+    int racingNumber;
+    String name;
+    int maxSpead;
+    double mileage;
+    String color;
+    double fuelLevel;
+    boolean damaged;
+
+
+
+
+
+}
